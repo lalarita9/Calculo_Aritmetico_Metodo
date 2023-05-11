f9291fd02d1663d9e7a9e430f48ec879c72d868e
Carolina Lara <lalarita@hotmail.com>
2023-05-11 18:09:56 -0400

    Primer y ultimo commit

---
+++ modulo_math.py
@@ -0,0 +1,14 @@
+#Requerimos hacer el cálculo de la raíz cuadrada de una variable llamada “y que tiene valor 81, 
+# utilizando el módulo de la librería math incorporada en Python. 
+# El resultado debemos asignarlo a una variable que será impresa en pantalla.
+
+#Se usa el modulo math e importa sqrt para obtener la raíz cuadrada
+from math import sqrt
+
+#Se crea variable con número a obtener raíz cuadrada
+numero = 81
+#Se realiza la función cálculo de raíz cuadrada
+resultado = sqrt(numero)
+#Se usa la función print para mostrar el resultado
+print(resultado)
+
